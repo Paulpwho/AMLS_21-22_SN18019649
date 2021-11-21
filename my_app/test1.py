35f312884baef74cca7deb5e5b61b2e076187c3c
Paul <paulpwho@gmail.com>
2021-11-21 00:24:53 +0000

    now puts data into dataframe

--- my_app/test1.py
+++ my_app/test1.py
@@ -1,4 +1,5 @@
-import pandas
+import pandas as pd
+from skimage import io
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
@@ -12,25 +13,34 @@ image_w = 512
 
 labels_path = './datasets/label.csv'
 image_path = './datasets/image/'
-filename = 'IMAGE_0000.jpg'
+#filename = 'IMAGE_0000.jpg'
 
-mri_labels = pandas.read_csv(labels_path)
+mri_labels = pd.read_csv(labels_path)
 mri_labels["label"] = mri_labels["label"] == "no_tumor"
 mri_labels["label"] = mri_labels["label"] * 1
 # no_tumour = 1, tumour = 0
 print(mri_labels)
 
-images = np.empty((0, image_h * image_w), np.uint8)
 
+print("Opening files")
+image_list = [] # [1]
 for filename in mri_labels["file_name"]:
-    im = np.array(Image.open(image_path + filename))
-    im = im[:, :, 1]
-    im = np.reshape(im, (1, -1))
-    images = np.append(images, im, axis=0)
-    print(filename)
+    image_list.append(io.imread(image_path + filename))
+
+print("Converting to np array")
+im = np.array(image_list)
+im = im[:, :, :, 1]
+
+num_samples = len(mri_labels["file_name"])
+im = np.reshape(im, (num_samples, -1))
+
+X = pd.DataFrame(im)
+Y = mri_labels["label"]
+
+print("Splitting data")
+xTrain, xTest, yTrain, yTest = train_test_split(X, Y, random_state=0)
+
+print("Done")
 
 
-print(images.shape)
-
-
-
+# [1] https://scikit-image.org/docs/dev/user_guide/getting_started.html
